dc2c996316a2bf40bd7cb25e2322cfae32e72f03
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-18 11:31:11 +0530

    Import ImageSlider component in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
+import ImageSlider from"./ImageSlider.js"
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <ImageSlider />
     </div>
   )
 }
