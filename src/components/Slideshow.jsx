f5d056cf543b44dcdc879f5e4db15f7e5be41f96
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-18 11:40:20 +0530

    Rename ImageSlider.jsx to Slideshow.jsx

---
+++ src/components/Slideshow.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+const Slideshow = () => {
+  const images = [
+    "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem/image/MQ6-0-image-20220419101504/01-mercedes-benz-the-new-eqs-suv-x296-2021-1080x1350.jpeg",
+    "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_546277676/image/MQ6-0-image-20220819111550/02-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg",
+    "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_546277676/image/MQ6-0-image-20220819111550/03-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg"
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <img
+        src={images[index]}
+        alt={`Image ${index + 1}`}
+        width="300"
+      />
+    </div>
+  );
+};
+
+export default Slideshow;
